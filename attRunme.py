519402a80ec7a339c3e74baa2b26f45c9605bca2
Aki78 <kmorooka78@gmail.com>
2019-03-17 14:08:17 +0200

    added att operator and sounds

--- attRunme.py
+++ attRunme.py
@@ -1,9 +1,11 @@
 import sys
-import random
+from pygame import mixer
 
+mixer.init()
 results = []
 counter = 0
-with open('Files/idontget.txt') as inputfile:
+outerCounter = 0
+with open('Files/attSentences.txt') as inputfile:
     for line in inputfile:
         results.append(line.strip().split(' '))
 
@@ -16,24 +18,29 @@ while True:
                 sys.stdout.write(words)
                 sys.stdout.write(" ")
             sys.stdout.write("\n")
+        outerCounter = 0
         if (count % 3) == 1:
-            randint = random.randint(0, len(line)-1)
             counter = 0
             for words in line:
-                if(counter == randint):
+                if('@' in words):
                     sys.stdout.write('___')
                     sys.stdout.write(" ")
+                    outerCounter = counter
                 else:
                     sys.stdout.write(words)
                     sys.stdout.write(" ")
                 counter += 1
             text = input(": ")
-            if(text==line[randint]):
+            if(text == line[outerCounter].replace("@", "")):
+                mixer.music.load("Sounds/Correct.mp3")
+                mixer.music.play()
                 sys.stdout.write("\033[1;36m")
-                print(text==line[randint])
+                print(text == line[outerCounter].replace("@", ""))
                 print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
             else:
+                mixer.music.load("Sounds/Wrong.mp3")
+                mixer.music.play()
                 sys.stdout.write("\033[1;31m")
-                print(text==line[randint])
-                print(line[randint])
+                print(text == line[outerCounter])
+                print(line[outerCounter])
                 print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
